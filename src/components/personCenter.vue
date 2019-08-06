3ce2ab7c0965448617ecc25144df808eea4450a3
justdj <977878634@qq.com>
2019-08-06 09:26:47 +0800

    修改一些琐碎的bug

--- src/components/personCenter.vue
+++ src/components/personCenter.vue
@@ -1,31 +1,31 @@
 <template>
-    <section style="width: 100%;height: 100%;" v-loading.fullscreen.lock="fullScreenLoading">
-      个人中心
-    </section>
+  <section style="width: 100%;height: 100%;" v-loading.fullscreen.lock="fullScreenLoading">
+    个人中心
+  </section>
 </template>
 
 <script>
-    export default {
-        name: "personCenter",
-      data(){
-          return {
-            fullScreenLoading:false
-          }
-      },
-      methods:{
-
-
-        openFullScreen() {
-          this.fullScreenLoading = true;
-          setTimeout(() => {
-            this.fullScreenLoading = false;
-          }, 200 + Math.random() * 150);
-        },
-      },
-      mounted(){
-          this.openFullScreen();
+  export default {
+    name: "personCenter",
+    data(){
+      return {
+        fullScreenLoading:false
       }
+    },
+    methods:{
+
+
+      openFullScreen() {
+        this.fullScreenLoading = true;
+        setTimeout(() => {
+          this.fullScreenLoading = false;
+        }, 200 + Math.random() * 150);
+      },
+    },
+    mounted(){
+      this.openFullScreen();
     }
+  }
 </script>
 
 <style scoped>
